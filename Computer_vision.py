cc7beddb6c402db09ccbd0242fb6675a86f45eca
deep-learning-engineer1 <148576909+deep-learning-engineer1@users.noreply.github.com>
2024-05-20 20:12:55 +0300

    Computer_vision.py

--- Computer_vision.py
+++ Computer_vision.py
@@ -1,3 +1,4 @@
+
 import cv2
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -10,31 +11,31 @@ from tensorflow.keras.applications.vgg16 import preprocess_input
 class Computer_Vision: # Here we need to build interface and switch on camera in this class.
     def __init__(self):
         self.open = True
-        self.window = Tk()
-        self.window.title("Computer Vision")
-        self.window.geometry("400x300")
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.label = Label()
-        self.label.pack()
-
     def mainloop(self):
         self.window.mainloop()
         
     def start_camera(self): # i have bug, when this function works, you can close it just when you close whole program, i could use asyncio function so it works together, and i can close it.
-        self.camera = cv2.VideoCapture(0)
-        while self.open:
-            self.ret, self.frame = self.camera.read()
-            self.frame = np.array(self.frame)
-            cv2.imshow('Video', self.frame)
-            cv2.waitKey(60)
-            return self.frame
-            
+        try:
+            self.camera = cv2.VideoCapture(0)
+            while self.open:
+                self.ret, self.frame = self.camera.read()
+                self.frame = np.array(self.frame)
+                cv2.imshow('Video', self.frame)
+                cv2.waitKey(60)
+                return self.frame
+        except:
+            print("Camera can't be opened.")
     def stop_camera(self):
-        self.open = False
-        self.camera.release()
-        print("Camera stopped successfully.")
-            
+        try:
+            self.open = False
+            self.camera.release()
+            print("Camera stopped successfully.")
+        except:
+            print("Camera can't be stopped.")
+        
+    def predict_bounding_box(self, frame, x_left, x_right, y_left, y_right, color, length):
+        self.rectangle(frame, (x_left, y_left), (x_right, y_right), color, length)
+        
 class Model:
     def __init__(self):
         try:
@@ -43,6 +44,7 @@ class Model:
             print("Model is found.")
         except:
             self.model = tf.keras.models.Sequential([
+               tf.keras.layers.ResizingLayer(width , height)
                tf.keras.layers.Conv2D(3, 3),
                tf.keras.layers.MaxPooling2D(32, 32),
                tf.keras.layers.Conv2D(3, 3),
@@ -53,10 +55,13 @@ class Model:
                tf.keras.layers.BatchNormalization(),
                tf.keras.layers.Dense(12, activation = 'relu'),
                tf.keras.layers.Dense(7, activation = 'relu'),
-               tf.keras.layers.Dense(8, activation = 'tanh')
+               
             ]) 
-            self.CV_model = tf.keras.Model(inputs=self.model,
-                                          outputs=[self.model, coordinates_output])
+            average_layer = tf.keras.layers.GlobalAveragePooling2D()(self.model)
+            class_output_layer = tf.keras.layers.Dense(8, activation='softmax')(average_layer)
+            coordinates_output = tf.keras.layers.Dense(5)(average_layer)
+            self.CV_model = tf.keras.Model(inputs=self.model.input,
+                                          outputs=[class_output_layer, coordinates_output])
             
             print("Building new model from scratch.")
         finally:
@@ -96,32 +101,13 @@ class Model:
         self.model.save('/Users/hliblukianov/Documents/computer_vision_model.keras')
         
     def predict_photo(self, frame):
+        
         prediction = self.model.predict(frame)
         print(prediction)
-        if prediction.any(6) > 0.5:
-            print("cucumber")
-        elif prediction.any(3) > 0.5:
-            print("blueberry")
-        elif prediction.any(8) > 0.5:
-            print("strawberry")
-        elif prediction.any(2) > 0.5:
-            print("banana")
-        elif prediction.any(1) > 0.5:
-            print("avocado")
-        elif prediction.any(7) > 0.5:
-            print("nut")
-        elif prediction.any(5) > 0.5:
-            print("cookie")
-        elif prediction.any(4) > 0.5:
-            print("bread")
-        else:
-            print("Not recognised")
 
 model = Model()
 
-
 model.prepare_data()
-
 
 model.train_model(model.dataset, model.test_dataset, model.validation_dataset)
 
@@ -134,5 +120,14 @@ while True:
     photo = photo.reshape((1, photo.shape[0], photo.shape[1], photo.shape[2]))
     photo = tf.image.rgb_to_grayscale(photo)
     model.predict_photo(photo)
-    
-cv.stop_camera() # For closing camera
+
+print(model.model.predict(model.dataset[0][0]))
+print(model.model.predict(model.dataset[1][0]))
+print(model.model.predict(model.dataset[2][0]))
+print(model.model.predict(model.dataset[3][0]))
+print(model.model.predict(model.dataset[4][0]))
+print(model.model.predict(model.dataset[5][0]))
+print(model.model.predict(model.dataset[6][0]))
+print(model.model.predict(model.dataset[7][0]))
+
+cv.stop_camera()
